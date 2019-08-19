3b9d93eb34ed372e9b823d72e28dd4882216b971
Tim Weiss <tdweiss1978@gmail.com>
2019-08-19 11:45:17 -0700

    First address-bloc checkpoint Complete

---
+++ app.js
@@ -0,0 +1,19 @@
+const inquirer = require('inquirer');
+const MenuController = require('./controllers/MenuController');
+const menu = new MenuController();
+
+menu.clear();
+menu.main();
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ controllers/MenuController.js
@@ -0,0 +1,75 @@
+const inquirer = require('inquirer');
+
+module.exports = class MenuController {
+  constructor() {
+    this.mainMenuQuestions = [
+      {
+           type: "list",
+           name: "mainMenuChoice",
+        message: "Please choose from an option below: ",
+        choices: [
+          "Add new contact",
+          "Exit"
+        ]
+      }
+    ];
+    this.contacts = [];
+  }
+
+ main() {
+    console.log("Welcome to AddressBloc!");
+    inquirer.prompt(this.mainMenuQuestions).then((response) => {
+       switch (response.mainMenuChoice) {
+         case "Add new contact":
+            this.addContact();
+            break;
+         case "Exit":
+           this.exit();
+           break;
+         default:
+           console.log('Invalid Input');
+           this.main();
+       }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+ }
+
+ clear() {
+  console.log("\x1Bc");
+ }
+
+exit () {
+  console.log("Thanks for using AddressBloc!");
+  process.exit();
+}
+
+addContact () {
+   this.clear();
+   console.log("addContact called");
+   this.main();
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
