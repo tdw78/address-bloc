e2ed255d915ffbd17b2aa87af1a8ba6d99769e58
Tim Weiss <tdweiss1978@gmail.com>
2019-08-20 12:11:29 -0700

    Problem with Jasmine

---
+++ spec/menu_controller_spec.js
@@ -0,0 +1,8 @@
+describe("MenuController", () => {
+  describe("#getContactCount()", () => {
+    it("should return 0 when no contacts are in the book", () => {
+      const menu = new MenuController();
+      expect(menu.getContactCount()).toBe(0);
+    });
+  });
+});
